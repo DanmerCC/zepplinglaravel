e04322453bfa926be6c18b5cabbc6717a9713d60
Danmer Ccoscco <danmerccosccogmail.com>
2022-08-31 18:29:10 -0500

    env path python executor

--- app/Http/Controllers/CustomSearchController.php
+++ app/Http/Controllers/CustomSearchController.php
@@ -71,7 +71,7 @@ class CustomSearchController extends Controller
         $newModel = new CustomSearch($request->only(['day', 'hour']));
         $newModel->save();
 
-        $command = "python3 ../pythonstore/prueba.py " . str_replace("-", "_", $newModel->day->format('Y-m-d')) . ' > ' . $newModel->id . '.log 2>&1 & echo $!; ';
+        $command = "python3 ".env('SCRIPT_PATH')." " . str_replace("-", "_", $newModel->day->format('Y-m-d')) . ' > ' . $newModel->id . '.log 2>&1 & echo $!; ';
         $pid = exec($command, $output);
         //dispatch(new RunNewCustomSearchJob($newModel));
         return $this->sendResponse($pid, "Tarea agregada");
