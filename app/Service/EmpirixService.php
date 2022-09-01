de7c66ec42351f612ebc605915930112471d7ada
Danmer Ccoscco <danmerccosccogmail.com>
2022-09-01 13:10:04 -0500

    dd

--- app/Service/EmpirixService.php
+++ app/Service/EmpirixService.php
@@ -25,7 +25,7 @@ class EmpirixService {
             'X-OpenAM-Username' => $this->username,
             'X-OpenAM-Password' => $this->password
         ])->post($this->baseurl . '/openam/json/authenticate', []);
-
+            dd($result->json());
         return $result->tokenId;
     }
 }
